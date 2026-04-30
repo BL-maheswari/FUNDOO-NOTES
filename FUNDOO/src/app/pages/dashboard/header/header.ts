4004a0dc32cebce0d2c0b2bf0fd35a08e868be07
BL-maheswari <ipperu.maheswari@bridgelabz.com>
2026-04-30 17:22:15 +0530

    Reapply "[Maheswari]:updated sidenav and header part"
    This reverts commit cee0d271ef49f51a0dad48bc4f00a00f460ab7e6.

--- FUNDOO/src/app/pages/dashboard/header/header.ts
+++ FUNDOO/src/app/pages/dashboard/header/header.ts
@@ -1,24 +1,30 @@
-import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatDividerModule } from '@angular/material/divider';
-// import { MatButtonModule } from '@angular/material/button';
-
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatIconModule,  MatToolbarModule,MatTooltipModule,MatMenuModule,
-  MatDividerModule,
-  MatButtonModule ],
+  imports: [
+    MatIconModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatMenuModule,
+    MatDividerModule,
+    MatButtonModule
+  ],
   templateUrl: './header.html',
-   styleUrl: './header.css',
+  styleUrl: './header.css',
 })
 export class Header {
 
+  @Output() menuClick = new EventEmitter<void>();
 
-
+  onMenuClick() {
+    this.menuClick.emit();
+  }
 }
--- FUNDOO/src/app/pages/sidenav/sidenav.ts
+++ FUNDOO/src/app/pages/sidenav/sidenav.ts
@@ -1,33 +1,28 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [RouterModule
-
-  ], 
+  imports: [RouterModule],
   templateUrl: './sidenav.html',
   styleUrls: ['./sidenav.css']
 })
 export class SidenavComponent {
 
-  expanded = true;
+  @Input() expanded = true;
+
   active = 'notes';
 
   @Output() menuChange = new EventEmitter<string>();
 
   navItems = [
-    { name: 'notes', label: 'Notes', icon: 'lightbulb',route:'/dashboard/notes' },
-    { name: 'reminders', label: 'Reminders', icon: 'notifications' ,route:'/dashboard/remainders'},
-    { name: 'labels', label: 'Labels', icon: 'label',route:'/dashboard/labels'},
-    { name: 'archive', label: 'Archive', icon: 'archive',route:'/dashboard/archive' },
-    { name: 'trash', label: 'Trash', icon: 'delete' ,route:'/dashboard/trash'}
+    { name: 'notes', label: 'Notes', icon: 'lightbulb', route: '/dashboard/notes' },
+    { name: 'reminders', label: 'Reminders', icon: 'notifications', route: '/dashboard/remainders' },
+    { name: 'labels', label: 'Labels', icon: 'label', route: '/dashboard/labels' },
+    { name: 'archive', label: 'Archive', icon: 'archive', route: '/dashboard/archive' },
+    { name: 'trash', label: 'Trash', icon: 'delete', route: '/dashboard/trash' }
   ];
-
-  toggle() {
-    this.expanded = !this.expanded;
-  }
 
   select(menu: string) {
     this.active = menu;
